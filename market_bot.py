af427e2cda396c831680cfa4fec01349cba3ef1c
alexander sichel <alexander sichel>
2017-05-25 19:52:32 -0400

    Complete clean Slack Print of Data

--- market_bot.py
+++ market_bot.py
@@ -9,6 +9,7 @@ def get_stock_data(symbol,two_days_prior = True):
     try:
         return web.DataReader (symbol, "yahoo", current_time, current_time)
     except:
+        print ("Yahoo Failed")
         try:
             return web.DataReader (symbol, "google", current_time, current_time)
         except:
@@ -27,15 +28,31 @@ def extract_symbol_from_input(input_message):
 def clean_up_df(df):
 # outputs string; price rounds to hundreds; string should look nice
     print(df)
-    dollar_columns = ['Open','High','Low','Close','Adj Close']
+    # Inolude ability to run Yahoo and Google Data
+    dollar_columns = ['Open','High','Low','Close']
     df[dollar_columns] = df [dollar_columns].apply(lambda num: round(num,2))
+    data_dict = df.to_dict()
+    clean_string = ''
+    for key in data_dict:
+        timestamp_dict = data_dict[key]
+        timestamp_key = list(timestamp_dict.keys())[0]
+        value = str(timestamp_dict [timestamp_key])
+        # clean_string += key + ': ' + value +'\n'
+        clean_string += '%s: %s\n' %(key,value)
+    return clean_string
+
+    print (clean_string)
+
+
+    # print (data_dict)
+
 
 
 def generate_output (input_message):
     symbol = extract_symbol_from_input(input_message)
     if symbol:
-        data = get_stock_data(symbol)
-        return str(data)
+        df = get_stock_data(symbol)
+        return clean_up_df(df)
     else:
         return "Loser"
 
