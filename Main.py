3bde3010bb5ffb4ddcaa11add6ffc1aa29f6380e
alexander sichel <alexandersichel@gmail.com>
2017-07-02 19:52:56 -0400

    Updates for hosting with Flask

--- Main.py
+++ Main.py
@@ -1,7 +1,17 @@
 from slack_api import *
+from flask import Flask
 
-url = get_websocket_url()
-connect (url)
+app = Flask (__name__)
+
+# @ = decorator setting up an API end point of the method below it
+@app.route ('/', methods = ['GET'])
+def main_page():
+    return 'hello world'
+
+app.run (host = '0.0.0.0', port = 3000)
+
+# url = get_websocket_url()
+# connect (url)
 
 # ws.send("Hello %d" % i)
 #
