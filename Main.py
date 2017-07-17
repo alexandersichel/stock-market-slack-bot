524530c7b9508d88d50084e12d11b8219f7bf8cf
alexander sichel <alexandersichel@gmail.com>
2017-07-16 21:23:49 -0400

    Webhook to send response to Slack.

--- Main.py
+++ Main.py
@@ -1,8 +1,11 @@
 from slack_api import *
 from flask import Flask, request
 import os
+from slacker import Slacker
+
 
 app = Flask (__name__)
+Slack_bot = Slacker(os.environ['HackerBotToken'])
 
 # @ = decorator setting up an API end point of the method below it
 @app.route ('/', methods = ['GET'])
@@ -14,6 +17,12 @@ def received_webhook ():
     data = request.form
     print ('Receiving Webhook Data')
     print (data)
+    channel_id = data.get('channel_id')
+    text = data.get ('text')
+    response = generate_output ('text')
+    Slack_bot.chat.post_message(channel_id, response)
+    return 'Data Sent'
+
 
 app.run (host = '0.0.0.0', port = os.environ.get("PORT",8080))
 
